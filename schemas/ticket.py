435e5e9ceb14df40551cdc1fd22a18a80786e7bb
cecillesalazar <cecillersalazar@gmail.com>
2020-11-22 11:21:17 -0800

    Added basic schema - will add more as needed

---
+++ schemas/ticket.py
@@ -0,0 +1,25 @@
+from ma import ma
+from models.tickets import TicketModel
+from models.tenant import TenantModel
+from models.user import UserModel
+from marshmallow import fields, validates, ValidationError
+
+class TicketSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = TicketSchema
+        include_fk = True
+
+    tenant = fields.Nested("TenantSchema")
+    user = fields.Nested("UserSchema")
+
+@validates("tenant")
+def validate_tenantID(self, value)
+    if not TenantModel.query.get(value):
+        raise ValidationError(f"{value} is not a valid tenant ID")
+
+@validates("assignedUser")
+@validates("sender")
+def validate_userID(self, value)
+    if not UserModel.query.get(value):
+        raise ValidationError(f"{value} is not a valid user ID")    
+
--- tests/unit/test_ticket.py
+++ tests/unit/test_ticket.py
@@ -1,31 +1,9 @@
-import pytest
 from tests.unit.base_interface_test import BaseInterfaceTest
 from models.tickets import TicketModel
 from schemas.tickets import TicketSchema
 
-lass TestBaseTicketModel(BaseInterfaceTest):
+class TestBaseTicketModel(BaseInterfaceTest):
     def setup(self):
         self.object = TicketModel()
         self.custom_404_msg = 'Ticket not found'
-
-        # TODO: Looks like we also don't have a schema - we need to add one
         self.schema = TicketSchema
-
-@pytest.mark.usefixtures('empty_test_db')
-class TestTicketModel():
-    def test_json(self, create_ticket):
-        ticket = create_ticket()
-        
-        # TODO: Set up necessary relationships - look at tickets.py
-        # Set up the tenant,assignedUser, and sender fields which map to the tenant and users models
-        # Also look at the notes relationship
-
-        # assert ticket.json() == {
-        #         'id': ticket.id,
-        #         'issue': ticket.issue,
-        #         'sender': ticket.sender,
-        #         'tenant': ticket.tenant,
-        #         'status': ticket.status,
-        #         'urgency': ticket.urgency,
-        #         'assignedUser': ticket.assignedUser,
-        #     }
