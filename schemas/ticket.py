c30015ac14614fe32187c351ca38db169203e774
Cecille Salazar <cecillersalazar@gmail.com>
2020-12-01 11:07:59 -0800

    WIP commit - schema test failing - Invalid input type

--- schemas/ticket.py
+++ schemas/ticket.py
@@ -4,6 +4,7 @@ from models.tenant import TenantModel
 from models.user import UserModel
 from marshmallow import fields, validates, ValidationError
 
+
 class TicketSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = TicketModel
@@ -14,12 +15,16 @@ class TicketSchema(ma.SQLAlchemyAutoSchema):
     sender = fields.Nested("UserSchema")
 
     @validates("tenant")
-    def validate_tenantID(self, value):
+    def validate_tenant(self, value):
         if not TenantModel.query.get(value):
             raise ValidationError(f"{value} is not a valid tenant ID")
 
     @validates("assignedUser")
+    def validate_assigned_user(self, value):
+        if not UserModel.query.get(value):
+            raise ValidationError(f"{value} is not a valid user ID")
+
     @validates("sender")
-    def validate_userIDs(self, value):
+    def validate_sender(self, value):
         if not UserModel.query.get(value):
             raise ValidationError(f"{value} is not a valid user ID")
